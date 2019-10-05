a1cea43ce3fcae6716f3045924842854c5cf5362
Eunjae Lee <karis612@gmail.com>
2019-10-06 00:37:39 +0200

    ask package name again

--- src/askPackageName.js
+++ src/askPackageName.js
@@ -2,6 +2,9 @@ import inquirer from 'inquirer';
 import validate from 'validate-npm-package-name';
 import available from 'npm-name';
 
+// eslint-disable-next-line no-console
+const print = console.log;
+
 export async function askPackageName() {
   const { packageName } = await inquirer.prompt([
     {
@@ -11,10 +14,24 @@ export async function askPackageName() {
     },
   ]);
   if (!validate(packageName).validForNewPackages) {
-    throw new Error(`"${packageName}" is not a valid package name.`);
+    print(`"${packageName}" is not a valid package name.`);
+    return await askPackageName();
   }
   if (!(await available(packageName))) {
-    throw new Error(`"${packageName}" already exists on npm.`);
+    const { choice } = await inquirer.prompt([
+      {
+        type: 'list',
+        name: 'choice',
+        message: `"${packageName}" already exists on npm.`,
+        choices: [
+          { name: 'Try a different name', value: 0 },
+          { name: 'Just keep using it.', value: 1 },
+        ],
+      },
+    ]);
+    if (choice === 0) {
+      return await askPackageName();
+    }
   }
   return packageName;
 }
