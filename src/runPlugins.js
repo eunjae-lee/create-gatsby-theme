c7acb67715319566029bb0817070a14e21fb8d15
Eunjae Lee <karis612@gmail.com>
2019-10-08 22:59:36 +0200

    fix: fix error when skipping plugins (#5)

--- src/runPlugins.js
+++ src/runPlugins.js
@@ -4,19 +4,23 @@ import ora from 'ora';
 export async function runPlugins({ plugins, opts }) {
   const answers = [];
   const results = [];
+  const shouldSkip = [];
   for (const [i, plugin] of plugins.entries()) {
     answers[i] = await inquirer.prompt(plugin.questions || []);
+  }
+
+  for (const [i, plugin] of plugins.entries()) {
+    shouldSkip[i] =
+      plugin.skipIf &&
+      typeof plugin.skipIf === 'function' &&
+      plugin.skipIf({ answers: answers[i], opts });
   }
 
   // eslint-disable-next-line no-console
   console.log('');
 
   for (const [i, plugin] of plugins.entries()) {
-    const skip =
-      plugin.skipIf &&
-      typeof plugin.skipIf === 'function' &&
-      plugin.skipIf({ answers: answers[i], opts });
-    if (!skip) {
+    if (!shouldSkip[i]) {
       const spinner = plugin.title ? ora(plugin.title).start() : undefined;
       results[i] = await plugin.run({ answers: answers[i], opts });
       if (spinner) {
@@ -29,7 +33,7 @@ export async function runPlugins({ plugins, opts }) {
   console.log('');
 
   for (const [i, plugin] of plugins.entries()) {
-    if (plugin.finished) {
+    if (!shouldSkip[i] && plugin.finished) {
       await plugin.finished({ answers: answers[i], result: results[i], opts });
     }
   }
