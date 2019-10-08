3aafe646638f1c59b9eef0ac99d7ea892be208b5
Eunjae Lee <karis612@gmail.com>
2019-10-08 22:25:44 +0200

    fix: fix error when skipping plugins

--- src/runPlugins.js
+++ src/runPlugins.js
@@ -8,15 +8,16 @@ export async function runPlugins({ plugins, opts }) {
     answers[i] = await inquirer.prompt(plugin.questions || []);
   }
 
+  const shouldSkip = (plugin, index) =>
+    plugin.skipIf &&
+    typeof plugin.skipIf === 'function' &&
+    plugin.skipIf({ answers: answers[index], opts });
+
   // eslint-disable-next-line no-console
   console.log('');
 
   for (const [i, plugin] of plugins.entries()) {
-    const skip =
-      plugin.skipIf &&
-      typeof plugin.skipIf === 'function' &&
-      plugin.skipIf({ answers: answers[i], opts });
-    if (!skip) {
+    if (!shouldSkip(plugin, i)) {
       const spinner = plugin.title ? ora(plugin.title).start() : undefined;
       results[i] = await plugin.run({ answers: answers[i], opts });
       if (spinner) {
@@ -29,7 +30,7 @@ export async function runPlugins({ plugins, opts }) {
   console.log('');
 
   for (const [i, plugin] of plugins.entries()) {
-    if (plugin.finished) {
+    if (plugin.finished && !shouldSkip(plugin, i)) {
       await plugin.finished({ answers: answers[i], result: results[i], opts });
     }
   }
